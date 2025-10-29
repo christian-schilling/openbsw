796e7410f5f01df42818cbdec92fd2f98d3dcbed
Christian Schilling <christian.schilling@accenture.com>
2025-10-29 13:57:59 +0100

    ws

---
+++ ws/run.sh
@@ -0,0 +1,7 @@
+#python3 .ci/build.py --preset CMAKE_PRESET --cxxid gcc
+set -e
+export FASTBUILD_CACHE_PATH=build/fbcache
+echo "\nBuilding...\n"
+cmake --preset CMAKE_PRESET -G "Unix Makefiles"
+cmake --build --preset CMAKE_PRESET -j 5 # -- -cache
+EXEC
